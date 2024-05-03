c466089bbd33f005a37a92849f7ea27f9584e326
yoi <ygarcialago@danielcastelao.org>
2024-05-03 09:54:26 +0200

    Constructor creado

--- src/cod/mvc/Coche.java
+++ src/cod/mvc/Coche.java
@@ -5,4 +5,10 @@ public class Coche {
     private String matricula;
     private String modelo;
     private Integer velocidad;
+
+    public Coche(String matricula, String modelo, Integer velocidad) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+    }
 }
