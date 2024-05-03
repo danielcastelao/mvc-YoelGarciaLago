4c58bc35c510f7d737f5ec21dc13d82c8ce2a467
yoi <ygarcialago@danielcastelao.org>
2024-05-03 09:58:13 +0200

    Método crearCoche creado

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -6,4 +6,13 @@ public class Model {
 
     ArrayList<Coche> parking = new ArrayList<>();
 
+    public Coche crearCoche (String matricula, String modelo, Integer velocidad){
+        Coche aux = null;
+        aux.setMatricula(matricula);
+        aux.setModelo(modelo);
+        aux.setVelocidad(velocidad);
+        parking.add(new Coche(aux.getMatricula(),aux.getModelo(),aux.getVelocidad()));
+        return aux;
+    }
+
 }
