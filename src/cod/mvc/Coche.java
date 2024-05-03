5c8804dc21bdee50fb682a2ac0516523eb8c732e
yoi <ygarcialago@danielcastelao.org>
2024-05-03 09:54:55 +0200

    Getters y setters creados

--- src/cod/mvc/Coche.java
+++ src/cod/mvc/Coche.java
@@ -11,4 +11,28 @@ public class Coche {
         this.modelo = modelo;
         this.velocidad = velocidad;
     }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Integer getVelocidad() {
+        return velocidad;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setVelocidad(Integer velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 }
