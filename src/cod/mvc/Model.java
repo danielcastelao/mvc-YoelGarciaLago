a0451e327552a8b97e9d4d6993a6c4a2cc3f0ff5
yoi <ygarcialago@danielcastelao.org>
2024-05-10 09:19:17 +0200

    Model corregido

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -1,48 +1,63 @@
 package cod.mvc;
 
+import cod.mvc.Coche;
+
 import java.util.ArrayList;
 
 public class Model {
-
     static ArrayList<Coche> parking = new ArrayList<>();
+    static Coche coche;
 
-    public static Coche crearCoche (String matricula, String modelo, Integer velocidad){
-        Coche aux = null;
-        aux.setMatricula(matricula);
-        aux.setModelo(modelo);
-        aux.setVelocidad(velocidad);
-        parking.add(new Coche(aux.getMatricula(),aux.getModelo(),aux.getVelocidad()));
-        return aux;
+    /**
+     * Metodo para crear Coches
+     * @param matricula del coche
+     * @param modelo del coche
+     * @param velocidad del coche
+     * @return devolvemos un coche
+     */
+    public static Coche crearCoche(String matricula, String modelo, int velocidad){
+        coche = new Coche(matricula,modelo,velocidad);
+        parking.add(coche);
+        return coche;
     }
 
-    public static Coche getCoche(String matricula){
-        Coche aux = null;
-        for(int i = 0; i < parking.size(); i++){
-            if(parking.get(i).getMatricula().equals(matricula)){
-                aux = parking.get(i);
-                break;
+    /**
+     * Metodo devolver Coche
+     * @param matricula del coche
+     * @return coche
+     */
+    public static Coche getCoche(String matricula) {
+        for (Coche coche : parking) {
+            if (coche.getMatricula().equals(matricula)) {
+                return coche;
             }
         }
-        return aux;
+        return null;
     }
 
-    public static void cambiarVelocidad(String matricula, Integer velocidad){
-        for (int i = 0; i < parking.size(); i++){
-            if(parking.get(i).getMatricula().equals(matricula)){
-                parking.get(i).setVelocidad(parking.get(i).getVelocidad()+velocidad);
-                break;
-            }
+    /**
+     * Metodo para cambiar la velocidad
+     * @param matricula del coche
+     * @param nuevaVelocidad nueva velovidad
+     */
+    public static void cambiarVelocidad(String matricula, int nuevaVelocidad) {
+        Coche coche = getCoche(matricula);
+        if (coche != null) {
+            coche.setVelocidad(nuevaVelocidad);
         }
     }
 
-    public static Integer getVelocidad(String matricula){
-        Integer aux = null;
-        for (int i = 0; i < parking.size(); i++){
-            if(parking.get(i).getMatricula().equals(matricula)){
-                aux =  parking.get(i).getVelocidad();
-            }
+    /**
+     * Metodo para devolver la Velocidad
+     * @param matricula del coche
+     * @return velocidad
+     */
+    public static int getVelocidad(String matricula) {
+        Coche coche = getCoche(matricula);
+        if (coche != null) {
+            return coche.getVelocidad();
         }
-        return aux;
+        return -1;
     }
 }
 
