4d08ad87c13a02abd30049b075de3f521278679f
yoi <ygarcialago@danielcastelao.org>
2024-05-31 09:45:15 +0200

    Creados nuevos métodos para la funcionalidad a añadir

--- src/com/cod/mvc/View/View.java
+++ src/com/cod/mvc/View/View.java
@@ -8,7 +8,17 @@ public class View {
         System.out.println(matricula + ": " + v + "km/hr");
         return true;
 
-
-
         }
+
+    /**
+     *
+     * @param list lista con todos los parámetros del coche que quieres mostrar
+     */
+    public static void mostrarTodo(String[] list){
+        System.out.println("Datos del coche:\nMatricula: " + list[0] + "\nVelocidad: " + list[2] + "\nModelo: " + list[1]);
+    }
+
+    public static void mensajeNulo(){
+        System.out.println("No existe el coche :p");
+    }
     }
